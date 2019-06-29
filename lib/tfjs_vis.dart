568c75ac09d46b46c4b6415e42c64c5d7726934e
Chatree Srichart <chatree.srichart@gmail.com>
2019-06-29 23:06:55 +0700

    Initialize scatterplot() method

--- lib/tfjs_vis.dart
+++ lib/tfjs_vis.dart
@@ -1,8 +1,34 @@
 /// Support for doing something awesome.
 ///
 /// More dartdocs go here.
+@JS()
 library tfjs_vis;
+
+import "package:js/js.dart";
 
 export 'src/tfjs_vis_base.dart';
 
 // TODO: Export any libraries intended for clients of this package.
+
+@JS()
+@anonymous
+class ScatterplotContainer {
+  dynamic name;
+}
+
+@JS()
+@anonymous
+class ScatterplotData {
+  dynamic values;
+}
+
+@JS()
+@anonymous
+class ScatterplotOpts {
+  dynamic xLabel;
+  dynamic yLabel;
+  dynamic height;
+}
+
+@JS("tfvis.render.scatterplot")
+external void scatterplot(ScatterplotContainer container, ScatterplotData data, ScatterplotOpts opts);
