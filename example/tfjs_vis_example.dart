f85bf89889d2d2f059c883a7e51e5911ed1ced3b
Chatree Srichart <chatree.srichart@gmail.com>
2019-06-29 22:46:23 +0700

    Initial commit

---
+++ example/tfjs_vis_example.dart
@@ -0,0 +1,6 @@
+import 'package:tfjs_vis/tfjs_vis.dart';
+
+main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/tfjs_vis.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library tfjs_vis;
+
+export 'src/tfjs_vis_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
