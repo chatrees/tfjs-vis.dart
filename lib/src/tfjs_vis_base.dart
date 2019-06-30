7f6492d765982c44af32729f15397062d552eab6
Chatree Srichart <chatree.srichart@gmail.com>
2019-06-30 13:29:59 +0700

    expose render object from tfjs_vis library

--- lib/src/tfjs_vis_base.dart
+++ lib/src/tfjs_vis_base.dart
@@ -1,6 +1,39 @@
+@JS('tfvis')
+library tfjs_vis;
+
+import "package:js/js.dart";
+
 // TODO: Put public facing types in this file.
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+@JS()
+@anonymous
+class ScatterplotContainer {
+  String name;
 }
+
+class ScatterplotValue {
+  num x;
+  num y;
+
+  ScatterplotValue(this.x, this.y);
+}
+
+@JS()
+@anonymous
+class ScatterplotData {
+  List<ScatterplotValue> values;
+}
+
+@JS()
+@anonymous
+class ScatterplotOptions {
+  String xLabel;
+  String yLabel;
+  num height;
+}
+
+class _Render {
+  external void scatterplot(ScatterplotContainer container, ScatterplotData data, ScatterplotOptions opts);
+}
+
+external _Render get render;
