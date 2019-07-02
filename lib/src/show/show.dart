12aa2ae809328f8116a6e1d13b61af8a3f736e0c
Chatree Srichart <chatree.srichart@gmail.com>
2019-07-02 22:13:41 +0700

    Create tfvis.show.fitCallbacks(..) method

--- lib/src/show/show.dart
+++ lib/src/show/show.dart
@@ -1,15 +1,35 @@
 @JS('tfvis')
 library show;
 
+import 'dart:core';
+
 import "package:js/js.dart";
 
-//FIXME:
-//import 'package:tfjs/tfjs.dart' as tf;
+import 'package:tfjs/tfjs.dart' as tf;
+import 'package:tfjs_vis/src/visor/visor.dart';
+
+@JS()
+@anonymous
+class FitCallbacksOptions {
+  List<String> callbacks;
+  bool zoomToFitAccuracy;
+  List<num> xAxisDomain;
+  List<num> yAxisDomain;
+  bool zoomToFit;
+  int width;
+  int height;
+  String xLabel;
+  String yLabel;
+  int fontSize;
+  String xType;
+  String yType;
+}
 
 @JS()
 @anonymous
 class Show {
-  external dynamic modelSummary(dynamic container, dynamic model);
+  external dynamic modelSummary(dynamic container, tf.Model model);
+  external dynamic fitCallbacks (Container container, List<String> metrics, FitCallbacksOptions opts);
 }
 
 @JS('show')
